44c48052345582f1bddd1828ddb9cd07a8629416
JamesShabo <jamesshabo@yahoo.com>
2017-03-01 11:28:25 -0800

    added menu buttons without styles

--- Components/Home.js
+++ Components/Home.js
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#eee'
+    backgroundColor: '#000'
   },
   largeText: {
     fontSize: 20,
--- Components/List.js
+++ Components/List.js
@@ -6,17 +6,47 @@ import { Button } from 'react-native-elements'
 class ButtonsList extends Component {
   render() {
     return(
-      <Button
-      style={styles.aboutUs}
-  title='About Us' />
+      <View>
+  <Button
+  style={styles.us}
+  raised
+  icon={{name: 'face'}}
+    title='About Us' />
 
+  <Button
+  style={styles.location}
+  raised
+  icon={{name: 'location-on'}}
+    title='Locations' />
+
+  <Button 
+  style={styles.keycodes}
+  raised
+  icon={{name:'vpn-key'}}
+    title='Generate Keycode' />
+
+  <Button 
+  style={styles.partner}
+  raised
+  icon={{name:'attach-money'}}
+    title='Partnership Opportunities' />
+  <Button 
+    style={styles.membership}
+    small
+    raised
+    icon={{name:'person'}}
+      title='Membership' />
+  
+
+  </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  aboutUs: {
-    color: '#fff',
+  us: {
+    color: '#639df9',
+    borderRadius: 5
   },
   red: {
     color: 'red',
